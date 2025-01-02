07bc34e76ebdc3c7486981bc40692bd22ceffa5b
developersharifuddin <sharif.uddin.9977@gmail.com>
2025-01-02 15:09:09 +0600

    Laravel & vue Shopify app

---
+++ resources/js/Components/Footer.vue
@@ -0,0 +1,23 @@
+<template>
+    <footer class="footer">
+      <p>&copy; 2025</p>
+    </footer>
+  </template>
+  
+  <script>
+  export default {
+    name: 'Footer',
+  };
+  </script>
+  
+  <style scoped>
+  .footer {
+    background-color:rgb(224, 224, 224);
+    color: rgb(46, 46, 46);
+    padding: 3px 0;
+    text-align: center;
+    bottom: 0 !important;
+    margin-top: 50px;
+  }
+  </style>
+  
---
+++ resources/js/Components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="dashboard-layout">
+    <header class="header">
+      <h3>Todo App</h3>
+    </header> 
+  </div>
+</template>
+  <script>
+  export default {
+    name: 'Header',
+  };
+  </script>
+ <style scoped>
+.header {
+  background-color: #dbdddb;
+  color: rgb(63, 62, 62); 
+  text-align: center;
+  padding : 1px 0;
+}
+</style>
